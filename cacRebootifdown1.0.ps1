89353cb15a2f44afca3123f9538e705338f3f0d7
telskamp <tomdabomb@gmail.com>
2017-02-12 16:39:03 +0100

    Initial commit
    Script  retreives all you server id`s and ip`s and pings them, if the
    server does not respond it wil initiate a reboot of that server

---
+++ cacRebootifdown1.0.ps1
@@ -0,0 +1,42 @@
+﻿
+$base = "https://panel.cloudatcost.com/api/v1"
+$cp = $base + "/cloudpro"
+$email = "your-cac-email-adress@here.com"
+$Apikey = "Your CAC Api key here"
+$login = "?key="+ $apikey+ "&login=" + $email
+$listservers = $base +"/listservers.php"+$login 
+$listtemplates = $base +"/listtemplates.php"+$login 
+$listtasks = $base +"/listtasks.php"+$login 
+$powerop = $base +"/powerop.php"
+$renameserver = $base +"/renameserver.php"+$login 
+$rdns = $base +"/rdns.php"+$login 
+$console = $base +"/console.php"+$login 
+$runmode = $base +"/runmode.php"+$login 
+$build = $cp +"/build.php"+$login
+$delete = $cp +"/delete.php"+$login 
+$resources = $cp +"/resources.php"+$login 
+
+
+
+#fetch list of servers
+$iplist = (Invoke-RestMethod -uri $listservers |Select-Object -ExpandProperty data |select ip, sid, hostname)
+#ing each server and reboot upon failure
+Foreach ($IP in $iplist){ 
+    if ((Test-NetConnection $IP.ip|Select-Object -ExpandProperty pingsucceeded) -eq -0){
+              Write-Output "ping for $ip failed trying to (re)boot"
+             $hostname = $ip.hostname
+             $ipa = $ip.ip
+
+                write-output "resetting $hostname with ip $ipa"
+
+                $body = @{ 
+key = $apikey
+login = $email
+sid = $IP.sid
+action = "reset"
+}
+                Invoke-RestMethod -uri $powerop -body $body -Method Post
+                     }
+    
+    }
+
